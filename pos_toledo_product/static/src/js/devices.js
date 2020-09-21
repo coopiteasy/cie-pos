a5ff2fa7e0e076919a99f92c9124f13a1787b639
Vincent Van Rossem <vvrossem@gmail.com>
2020-09-21 15:10:18 +0200

    [ADD] pos modules used by vracoop : - pos_customer_display - pos_customer_display_currency - pos_hash_cert - pos_toledo_container - pos_toledo_product
    No need to copy `pos_container` since a PR is opened on `oca/pos`
    (https://github.com/OCA/pos/pull/498)
    
    Moved from https://github.com/coopiteasy/pos/tree/12.0-cie-vracoop.
    `coopiteasy/pos` is forked from `OCA/pos`. The latter repository
    has evolved, new modules are available. The 12.0-cie-vracoop on the
    former repository cannot be maintained anymore.

---
+++ pos_toledo_product/static/src/js/devices.js
@@ -0,0 +1,85 @@
+/*
+    Copyright 2019 Coop IT Easy SCRLfs
+    	    Robin Keunen <robin@coopiteasy.be>
+    	    Vincent Van Rossem <vvrossem@gmail.com>
+    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+*/
+
+odoo.define('pos_toledo_product.devices', function (require) {
+    "use strict";
+
+    var devices = require('point_of_sale.devices');
+
+    devices.ProxyDevice.include({
+        reset_weight_product: function () {
+            var ret = new $.Deferred();
+            this.message('reset_weight').then(function (status) {
+                ret.resolve(status)
+            }, function () {
+                ret.resolve({weight: 0.0, price: 0.0, unit: 'kg', info: 'ko'})
+            });
+            return ret;
+        },
+
+        reset_tare: function () {
+            var price = '001000'; // bizerba doesn't accept '000000' as unit price
+            var ret = new $.Deferred();
+            this.message('scale_price', {price: price}).then(function (weight) {
+                ret.resolve(weight);
+            }, function () {
+                ret.resolve({weight: 0.0, price: 0.0, unit: 'kg', info: 'ko'})
+            });
+            return ret;
+        },
+
+        scale_read_data_price_product: function (price) {
+            var self = this;
+            var ret = new $.Deferred();
+            if (self.use_debug_weight) {
+                return (new $.Deferred()).resolve({
+                    weight: this.debug_weight,
+                    price: this.debug_price,
+                    unit: 'kg',
+                    info: 'ok'
+                });
+            }
+            this.message('scale_price', {price: price})
+                .then(function (weight) {
+                    ret.resolve(weight);
+                }, function () {
+                    ret.resolve({weight: 0.0, price: 0.0, unit: 'kg', info: 'ko'});
+                });
+            return ret;
+        },
+
+        scale_read_data_price_tare_product: function (price, tare) {
+            var self = this;
+            var ret = new $.Deferred();
+            if (self.use_debug_weight) {
+                return (new $.Deferred()).resolve({
+                    weight: this.debug_weight,
+                    price: this.debug_price,
+                    unit: 'kg',
+                    info: 'ok'
+                });
+            }
+            this.message('scale_price_tare', {price: price, tare: tare})
+                .then(function (weight) {
+                    ret.resolve(weight);
+                }, function () {
+                    ret.resolve({weight: 0.0, price: 0.0, unit: 'kg', info: 'ko'});
+                });
+            return ret;
+        },
+
+        debug_set_weight: function (kg) {
+            this._super(kg);
+            this.debug_price = NaN;
+        },
+
+        debug_reset_weight: function () {
+            this._super();
+            this.debug_price = 0.0;
+        },
+    });
+});
