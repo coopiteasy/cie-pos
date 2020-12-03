7f02f228ed7e3df1ca0c362fa02b7af6b3d0f374
Vincent Van Rossem <vvrossem@gmail.com>
2020-12-03 17:38:49 +0100

    [IMP] pos_hash_cert : _logger.info for better debugging

--- pos_hash_cert/models/pos_hash_cert.py
+++ pos_hash_cert/models/pos_hash_cert.py
@@ -1,37 +1,38 @@
 import logging
 import os
 
+from checksumdir import dirhash
+
 from odoo import api, fields, models
 from odoo.tools.config import config
 
-from checksumdir import dirhash
-
 _logger = logging.getLogger(__name__)
 
-CERT_DIR = config.get('certified_modules_directory', 'pos_certified_modules')
+CERT_DIR = config.get("certified_modules_directory", "pos_certified_modules")
 USER_DIR = os.path.expanduser("~")
 
 
 class ModuleHash(models.Model):
-    _inherit = 'ir.module.module'
+    _inherit = "ir.module.module"
 
-    hash = fields.Char(compute='_compute_hash',
-                       help='Module hash')
+    hash = fields.Char(compute="_compute_hash", help="Module hash")
 
     @api.multi
     def _compute_hash(self):
 
-        _logger.debug("[pos_hash_cert] USER_DIR = %s" % USER_DIR)
+        _logger.info("[pos_hash_cert] USER_DIR = %s" % USER_DIR)
         start_dir = os.path.dirname(os.path.realpath(__file__))
-        _logger.debug("[pos_hash_cert] start_DIR = %s" % start_dir)
+        _logger.info("[pos_hash_cert] start_DIR = %s" % start_dir)
 
         last_root = start_dir
         current_root = start_dir
         found_cert_dir = None
 
-        while found_cert_dir is None and current_root != os.path.dirname(USER_DIR):
+        while found_cert_dir is None and current_root != os.path.dirname(
+            USER_DIR
+        ):
             pruned = False
-            for root, dirs, files in os.walk(current_root):
+            for root, dirs, _ in os.walk(current_root):
                 if not pruned:
                     try:
                         # Remove the part of the tree we already searched
@@ -48,7 +49,9 @@ class ModuleHash(models.Model):
             current_root = os.path.dirname(last_root)
 
         if found_cert_dir:
-            _logger.debug("[pos_hash_cert] found_cert_dir = %s" % found_cert_dir)
+            _logger.info(
+                "[pos_hash_cert] found_cert_dir = %s" % found_cert_dir
+            )
             certified_modules = [
                 name
                 for name in os.listdir(found_cert_dir)
@@ -57,7 +60,15 @@ class ModuleHash(models.Model):
 
             for record in self:
                 if record.name in certified_modules:
-                    record.hash = dirhash(found_cert_dir, 'sha256', excluded_extensions=['pyc'])
+                    record.hash = dirhash(
+                        found_cert_dir, "sha256", excluded_extensions=["pyc"]
+                    )
+                    _logger.info(
+                        "[pos_hash_cert] '%s' with hash '%s'"
+                        % (record.name, record.hash)
+                    )
         else:
-            _logger.debug("[pos_hash_cert] no certified modules directory found")
+            _logger.info(
+                "[pos_hash_cert] no certified modules directory found"
+            )
             pass
