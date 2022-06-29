8a3ba2c3b6593f44362c38d69ac3b8009cd4aebf
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-29 11:29:52 +0200

    [FIX] SCRLfs → SC
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- pos_print_receipt_backend/__manifest__.py
+++ pos_print_receipt_backend/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2020 - Today Coop IT Easy SCRLfs
+# Copyright 2020 - Today Coop IT Easy SC
 #     Vincent Van Rossem <vincent@coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Generate POS receipts from Odoo Backend",
     "version": "12.0.1.1.0",
-    "author": "Coop IT Easy SCRLfs",
+    "author": "Coop IT Easy SC",
     "website": "https://coopiteasy.be",
     "license": "AGPL-3",
     "category": "Point of Sale",
